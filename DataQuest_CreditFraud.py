7459f279325569a51e63e2bbf81e3ebf69446d7f
Jazzzzz <108416889+Jazzzzzm@users.noreply.github.com>
2024-04-06 00:33:37 -0400

    Update DataQuest_CreditFraud.py
    Added modules
    Added get_mae function
    Added train_data and test_data

--- DataQuest_CreditFraud.py
+++ DataQuest_CreditFraud.py
@@ -1,8 +1,31 @@
+
 import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+
+def get_mae(max_nodes, train_x, val_x, train_y, val_y):
+    total = 0
+    model = DecisionTreeRegressor(max_leaf_nodes = max_nodes, random_state = 13)
+    model.fit(train_x, train_y)
+    model_Predictions = model.predict(val_x)
+    for item in model_Predictions:
+        total += item
+    results = total/len(model_Predictions)
+    MAE = [mean_absolute_error(val_y, model_Predictions), results]
+
+    return (MAE)
 
 train = pd.read_csv("data_train.csv")
 test = pd.read_csv("data_test.csv")
 
-print(train.head())
-print(test.head())
+train_data = train.dropna()
+test_data = test.dropna()
+
+print(train_data.head())
+print(test_data.head())
+
+y = train_data.isFraud
 
