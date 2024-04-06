96fe6533dfa24e7589dfe8346693ffb0021abba6
Jazzzzz <108416889+Jazzzzzm@users.noreply.github.com>
2024-04-06 02:23:47 -0400

    Update DataQuest_CreditFraud.py
    Deleted functions for line regression
    Encoded variables for fit
    Added simulation for model fit

--- DataQuest_CreditFraud.py
+++ DataQuest_CreditFraud.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import scipy.optimize as so
+import itertools as it
 
 
 from sklearn.model_selection import train_test_split
@@ -23,92 +24,47 @@ def get_mae(max_nodes, train_x, val_x, train_y, val_y):
 
     return (MAE)
 
-def simpleRegPredict(b,x):
-    yp=b[0]+b[1]*x
-    return yp
-
-def simpleRegLossRSS(b,x,y):
-    predY = simpleRegPredict(b,x)
-    res = y-predY
-    rss = sum(res**2)
-    deriv = np.zeros(2)
-    deriv[0]=-2*sum(res)
-    deriv[1]=-2*sum(res*x)
-    return (rss,deriv)
-
-def simpleRegFit(x,y,lossFcn=simpleRegLossRSS):
-    b0=[np.mean(y),0]
-    RES = so.minimize(lossFcn,b0,args=(x,y),jac=True)
-    b=RES.x # Results
-    res = y-np.mean(y)
-    TSS = sum(res**2)
-    RSS,grad = simpleRegLossRSS(b,x,y)
-    R2 = 1-RSS/TSS
-    return (R2,b)
-
-def addPrediction(b,x,predFunc,ax,linestyle='r:'):
-    xrange = max(x)-min(x)
-    xp = np.linspace(min(x)-xrange/30,max(x)+xrange/30,num=50)
-    yp = predFunc(b,xp)
-    ax.plot(xp,yp,linestyle)
-    return(ax)
-
-def multRegPredict(b,D,xname):
-    yp=np.ones(len(D.index))*b[0]        
-    for i in range(len(xname)):
-        yp=yp+D[xname[i]]*b[i+1]        
-    return yp
-
-def multRegLossRSS(b,D,y,xname):
-    predY = multRegPredict(b,D,xname)
-    res = y-predY
-    rss = sum(res**2)
-    grad=np.zeros(len(b))
-    grad[0]=-2*np.sum(res)
-    for i in range(len(xname)):
-        grad[i+1]=-2*np.sum(D[xname[i]]*res)
-    return (rss,grad)
-
-def multRegFit(D,y,xname):
-    N=len(xname)
-    b0=np.zeros((N+1,))
-    RES = so.minimize(multRegLossRSS,b0,args=(D,y,xname),
-                    jac=True,
-                    tol=1e-3)
-    if (not(RES.success)):
-        print('unsuccessful fit')
-        print(RES)
-    b=RES.x # Results
-    res = y-np.mean(y)
-    TSS = sum(res**2)
-    RSS,deriv = multRegLossRSS(b,D,y,xname)
-    R2 = 1-RSS/TSS
-    return (R2,b)
-
-def bootstrapReg(D,y,args,fitfcn,numIter=1000):
-    r2,b=fitfcn(D,y,args)
-    numParams=len(b)
-    N = len(D.index)
-    ind  = np.arange(N)
-    stats = np.zeros((numIter,numParams))
-    for i in range(numIter):
-        sample=np.random.choice(ind,N)
-        r2,stats[i,:]=fitfcn(D.iloc[sample],y[sample],args)
-    return stats
-
-def confidenceInt(data,perc):
-    Int=(np.percentile(data,(100-perc)/2),np.percentile(data,100-(100-perc)/2))
-    return Int
-
 train = pd.read_csv("data_train.csv")
 test = pd.read_csv("data_test.csv")
 
 train_data = train.dropna()
 test_data = test.dropna()
 
-print(train_data.head())
-print(test_data.head())
+categories = {
+    "misc_net" : 1,
+    "shopping_net" : 2,
+    "misc_pos" : 3,
+    "grocery_pos" : 4,
+    "entertainment" : 5,
+    "gas_transport" : 6,
+    "personal_care" : 7,
+    "shopping_pos" : 8,
+    "food_dining" : 9,
+    "home" : 10,
+    "kids_pets" : 11,
+    "grocery_net" : 12,
+    "health_fitness" : 13,
+    "travel" : 14
+} 
 
-y = train_data.isFraud
+train_data['transDate'] = pd.to_datetime(train_data['transDate'])
+train_data['year'] = train_data['transDate'].dt.year
+train_data['month'] = train_data['transDate'].dt.month
+train_data['day'] = train_data['transDate'].dt.day
+
+train_data["transDate_num"] = train_data['day']*10000 + train_data["month"]*100 + train_data["year"]
+
+train_data.category = [categories[item] for item in train_data.category]
+
+train_data["diff_Long"] = abs(train_data["longitude"] - train_data["merchLongitude"])
+
+train_data["diff_Lat"] = abs(train_data["longitude"] - train_data["merchLongitude"])
+
+list_var = ["category", "amount", "diff_Long", "diff_Lat"]
+
+for max_node in [10, 100, 1000, 10000]:
+    train_x, val_x, train_y, val_y = train_test_split(train_data[list_var], train_data["isFraud"], random_state=13)
+    my_mae = get_mae(max_node, train_x, val_x, train_y, val_y)
+    print(f"Max nodes {max_node} \t\t Mean Abs Error: {my_mae[0]} \t\t Avg Predict: {my_mae[1]}")
 
 
